60946ec36c291be3fb41595f973210810ebf71f6
Bruno Lorente Cantarero <blorente@novadevs.com>
2025-10-15 21:55:19 +0200

    Update soon page.

--- src/pages/SoonPage.tsx
+++ src/pages/SoonPage.tsx
@@ -1,41 +1,20 @@
-//import black from "../assets/images/shilling-logo/large/black.svg";
-import blue from "../assets/images/shilling-logo/large/blue.svg";
-import green from "../assets/images/shilling-logo/large/green.svg";
-import orange from "../assets/images/shilling-logo/large/orange.svg";
-import purple from "../assets/images/shilling-logo/large/purple.svg";
-import red from "../assets/images/shilling-logo/large/red.svg";
-import white from "../assets/images/shilling-logo/large/white.svg";
+import allColors from "../assets/images/shilling-logo/all-colors.gif"
+import allColorsMobile from "../assets/images/shilling-logo/all-colors-mobile.gif"
 
 const SoonPage = () => {
     return (
-        <div className="static w-screen h-screen">
-            <a href="https://x.com/shillingdotfun">
-                <div className="grid sm:grid-cols-2 grid-cols-1">
-                    <img
-                        src={white}
-                        alt="shilling.fun logo"
-                    />
-                    <img
-                        src={blue}
-                        alt="shilling.fun logo"
-                    />
-                    <img
-                        src={green}
-                        alt="shilling.fun logo"
-                    />
-                    <img
-                        src={purple}
-                        alt="shilling.fun logo"
-                    />
-                    <img
-                        src={red}
-                        alt="shilling.fun logo"
-                    />
-                    <img
-                        src={orange}
-                        alt="shilling.fun logo"
-                    />
-                </div>
+        <div className="fixed inset-0 w-screen h-screen overflow-hidden">
+            <a href="https://x.com/shillingdotfun" className="block w-full h-full">
+                <img
+                    src={allColorsMobile}
+                    alt="shilling.fun logo"
+                    className="block md:hidden w-full h-full object-cover"
+                />
+                <img
+                    src={allColors}
+                    alt="shilling.fun logo"
+                    className="hidden md:block w-full h-full object-cover"
+                />
             </a>
         </div>
     )
